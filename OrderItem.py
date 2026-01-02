a5ca9d6d07c8f19cdd0df8e5a2080a2c7e778c89
Shantanu Shukla <shantanushukla006@gmail.com>
2026-01-02 12:34:58 -0500

    Add: OrderItem Methods

--- OrderItem.py
+++ OrderItem.py
@@ -8,3 +8,14 @@ class OrderItem:
     ):
         self.product = product
         self.quantity = quantity
+        self.unit_price = product.price
+
+    def get_subtotal(self):
+        return self.unit_price * self.quantity
+
+    def __repr__(self) -> str:
+        return (
+            f"OrderItem(product={self.product.name}, "
+            f"quantity={self.quantity}, "
+            f"unit_price={self.unit_price:.2f})"
+        )
