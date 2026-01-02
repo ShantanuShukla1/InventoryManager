8f6b01b10153f0829ee9667008f2675b33379a8a
Shantanu Shukla <shantanushukla006@gmail.com>
2026-01-02 12:34:49 -0500

    Edit: OrderItem Class

--- OrderItem.py
+++ OrderItem.py
@@ -1,6 +1,10 @@
 from Product import Product
 
 class OrderItem:
-    def __init__(self, product: Product, quantity: int):
+    def __init__(
+            self,
+            product: Product,
+            quantity: int
+    ):
         self.product = product
         self.quantity = quantity
