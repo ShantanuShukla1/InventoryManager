7054a197e856ad99399ffc37f27c5315b78b7054
Shantanu Shukla <shantanushukla006@gmail.com>
2026-01-02 12:35:11 -0500

    Add: Customer Methods

--- Customer.py
+++ Customer.py
@@ -12,3 +12,20 @@ class Customer:
         self.name = name
         self.email = email
         self.orders = orders
+
+    def place_order(self, order):
+        self.orders.append(order)
+
+    def get_order_history(self):
+        return self.orders
+
+    def get_total_spent(self) -> float:
+        """Return total money spent by the customer."""
+        return sum(order.calculate_total() for order in self.orders)
+
+    def __repr__(self) -> str:
+        return (
+            f"Customer(id={self.customer_id}, "
+            f"name='{self.name}', "
+            f"email='{self.email}')"
+        )
